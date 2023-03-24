8170717b2b4a491c1c828d016ed8f104a2786ba6
Zeizil <ekphilbrook@gmail.com>
2023-03-24 18:21:44 -0400

    added to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,28 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE movies_db;
+USE employees_db;
 
-CREATE TABLE movies (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
     review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (department_id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    ON DELETE CASCADE
 );
